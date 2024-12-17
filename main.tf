0b4d9ffd6ef319047c5c870e9626548ca30d313d
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-17 14:31:12 +0200

    Create lambda functions

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
-variable "function_name" {
-  type = string
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "lambda-user"
 }
 
-data "aws_lambda_function" "existing" {
-  function_name = var.function_name
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function.zip"
+  function_name = "lambda_handler"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "lambda_function.lambda_handler"
+
+  source_code_hash = filebase64sha256("lambda_function.zip")
+
+  runtime = "python3.13"
+
 }
