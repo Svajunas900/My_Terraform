ceede9f6fbc4b2e6c316a99226de11cc972c9e4c
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-17 15:34:13 +0200

    Modify code

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,7 @@
+from botocore.vendored import requests
+
+
 def lambda_handler(event, context):
-    print("Hello, World!")
-    return {
-        'statusCode': 200,
-        'body': "Hello from Lambda!"
-    }
+    response = requests.get("https://127.0.0.1:8000/prices/msft")
+    print(response.json())
+    return response.json()
