cb4c2f5d0c61ac20276eab288d36f8cde72b4c0f
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-17 14:55:17 +0200

    Modify code

--- main.tf
+++ main.tf
@@ -32,6 +32,6 @@ resource "aws_lambda_function" "test_lambda" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.assume_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
