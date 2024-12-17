324c8dfed1c23e02b4c244ecb08992e3d518cb04
Svajunas900 <svajunaskoncius11@gmail.com>
2024-12-17 14:54:06 +0200

    Modify code

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "lambda-user"
    assume_role_policy = data.aws_iam_policy_document.assume_role.json
@@ -15,4 +29,9 @@ resource "aws_lambda_function" "test_lambda" {
 
   runtime = "python3.13"
 
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
